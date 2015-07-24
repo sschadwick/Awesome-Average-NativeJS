75a337c5b9ac4f1a5bbc05222a250ada4cee9878
“sschadwick” <sschadwick@gmail.comssh-keygen -t rsa -C sschadwick@gmail.com>
2015-07-24 11:21:05 -0700

    commenting code

--- awesome index.js
+++ awesome index.js
@@ -1,25 +1,43 @@
-var information = [{
-	name: 'Bob',
-	occupation: 'programmer',
-	awesomeIndex: 7,
+var information = [
+{
+name: 'Trinity',
+occupation: 'programmer',
+awesomeIndex: 10
 },
-{	name: 'Alice',
-	occupation: 'programmer',
-	awesomeIndex: 9,
+{
+name: 'Mater',
+occupation: 'tow truck'
 },
-{	name: 'Zaphod',
-	occupation: 'President of the Galaxy'
-}];
+{
+name: 'Number 5',
+occupation: 'programmer',
+awesomeIndex: 7
+},
+{
+name: 'Alice',
+occupation: 'programmer',
+awesomeIndex: 9
+},
+{
+name: 'Zaphod',
+occupation: 'President of the Galaxy'
+},
+{
+name: 'Bob Parr',
+occupation: 'programmer',
+awesomeIndex: 6
+} ];
 
 var programmers = 0;
 var awesomeTotal = 0;
 var awesomeAverage = 0;
 
+//find the sum and number of people with awesomeIndex
 for (var i=0; i<information.length; i++){
 	if (information[i].occupation === 'programmer'){
 		    awesomeTotal += information[i].awesomeIndex;
 		    programmers++;
 	}
-	awesomeAverage = awesomeTotal / programmers;
+	awesomeAverage = awesomeTotal / programmers; //find average awesomeIndex
 }
-document.getElementById("awesome-index").value = awesomeAverage;
+$('#awesome-index').text(awesomeAverage); //pass average to HTML file
